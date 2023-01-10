2f4f30f6aab96e5456584ae6d0d0c42c0c7ef39c
MOFA01 <mouad01fakihi@gmail.com>
2023-01-10 22:04:39 +0100

    second commit

---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: 'Twist Dining Table',
+      slug: 'Twist-Dining-Table',
+      category: 'Home Fourniture',
+      subCategory: 'Dining',
+      image: '/assets/images/TwistDiningTable.jpg',
+      price: 288,
+      counInStock: 9,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+    {
+      name: 'Optima High Back Chair',
+      slug: 'Optima-High-Back-Chair',
+      category: 'Office Fourniture',
+      subCategory: 'Office Chair',
+      image: '/assets/images/OptimaHighBackChair.jpg',
+      price: 211,
+      counInStock: 23,
+      rating: 4.0,
+      numReviews: 10,
+      description:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+    {
+      name: 'Broma Bed Side Table',
+      slug: 'Broma-Bed-Side-Table',
+      category: 'Home Fourniture',
+      subCategory: 'Bedroom',
+      image: '/assets/images/BromaBedSideTable.jpg',
+      price: 211,
+      counInStock: 23,
+      rating: 4.2,
+      numReviews: 14,
+      description:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+    {
+      name: 'Developer Chair',
+      slug: 'Developer-Chair',
+      category: 'Office Fourniture',
+      subCategory: 'Office Chair',
+      image: '/assets/images/DeveloperChair.jpg',
+      price: 211,
+      counInStock: 5,
+      rating: 4.8,
+      numReviews: 10,
+      description:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+  ],
+};
+
+export default data;
